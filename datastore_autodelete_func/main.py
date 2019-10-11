10807e047c4d46bad3424271714ec51c628eac11
dvanrhenen <d.vanrhenen@vwt.digital>
2019-10-11 13:53:47 +0200

    Updated function to delete in batch. Changed datetime format. DAT-1036

--- datastore_autodelete_func/main.py
+++ datastore_autodelete_func/main.py
@@ -9,17 +9,22 @@ def auto_delete(request):
         'interval' in request.args:
 
         db_client = datastore.Client()
+        batch = db_client.batch()
         query = db_client.query(kind=request.args['kind'])
 
         interval = int(request.args['interval'])
         logging.info(f'Auto-deleting entities older than {interval} days')
 
-        time_delta = datetime.datetime.utcnow() - datetime.timedelta(days=interval)
+        time_delta = (datetime.datetime.now() - datetime.timedelta(days=interval)).isoformat()
         query.add_filter(request.args['field'], '<=', time_delta)
         query.keys_only()
         entities = query.fetch()
 
-        db_client.delete_multi(entities)
+        batch.begin()
+        for entity in entities:
+            batch.delete(entity.key)
+
+        batch.commit()
     else:
         problem = {'type': 'MissingParameters',
                    'title': 'Expected kind, days interval and field for deleting entities not found',
